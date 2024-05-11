6cf8e8f931d5f4a32423cbfde095fd9822b571f1
navy <yvanrobert974@gmail.com>
2024-05-11 10:23:11 +0200

    ajout 2

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,7 +1,10 @@
 import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/home", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/connexion", "Connexion", "/pages/connexion.html"),
+    new Route("/inscription", "Inscription", "/pages/inscription.html")
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
