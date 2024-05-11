1c94f08b8f196dd8471b81e16750acd64b7f5e4b
navy <yvanrobert974@gmail.com>
2024-05-11 11:40:30 +0200

    ajout pages reservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,10 +3,4 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/home", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/connexion", "Connexion", "/pages/connexion.html"),
-    new Route("/inscription", "Inscription", "/pages/inscription.html"),
-    new Route("/compte", "Mon compte", "/pages/compte.html"),
-    new Route("/editPassword", "Changement de mot de passe", "/pages/editPassword.html")
-];
-//Le titre s'affiche comme ceci : Route.titre - websitename
-export const websiteName = "Quai Antique";
+    new Route("/connexion", "Connexio
