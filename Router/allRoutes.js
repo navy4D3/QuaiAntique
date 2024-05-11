f41fa3528f3e7306fc0537440a8222e75286096f
navy <yvanrobert974@gmail.com>
2024-05-11 11:22:43 +0200

    ajout edition/suppression modal galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,9 @@ export const allRoutes = [
     new Route("/home", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/connexion", "Connexion", "/pages/connexion.html"),
-    new Route("/inscription", "Inscription", "/pages/inscription.html")
+    new Route("/inscription", "Inscription", "/pages/inscription.html"),
+    new Route("/compte", "Mon compte", "/pages/compte.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/editPassword.html")
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
